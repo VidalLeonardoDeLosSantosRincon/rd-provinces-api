09d6c29a4b70b15852e145469cb71e03b6835042
Vidal Leonardo De Los Santos Rincon <niggbibi@hotmail.com>
2020-10-09 12:46:13 -0400

    error with name of methods simplifieApiResponse and setApiResponseLang fixed

--- index.js
+++ index.js
@@ -4,31 +4,31 @@
     const port = process.env.PORT || 3050;
     //const connection = require("./DBconnection");
     const {getAllProvinces, getProvinceByCode, getProvinceByName} = require("./provinces");
-    const { setApiReponseLang } = require("./services/setApiResponseLang");
-    const { simplifiedApiReponse} = require("./services/simplifiedApiResponse");
-    
+    const { setApiResponseLang } = require("./services/setApiResponseLang");
+    const { simplifiedApiResponse} = require("./services/simplifiedApiResponse");
+
     app.use(express.json());
 
     //routes
     app.get("/", (request, response)=>{
-        let lang = setApiReponseLang(request.query.lang);
-        let simplified = simplifiedApiReponse(request.query.simplified);
+        let lang = setApiResponseLang(request.query.lang);
+        let simplified = simplifiedApiResponse(request.query.simplified);
         response.send(
             getAllProvinces(lang, simplified)
         );
     });
 
     app.get("/provinces", (request, response)=>{
-        let lang = setApiReponseLang(request.query.lang);
-        let simplified = simplifiedApiReponse(request.query.simplified);
+        let lang = setApiResponseLang(request.query.lang);
+        let simplified = simplifiedApiResponse(request.query.simplified);
         response.send(
             getAllProvinces(lang, simplified)
         );          
     });
 
     app.get("/provinces/:code", (request, response)=>{
-        let lang = setApiReponseLang(request.query.lang);
-        let simplified = simplifiedApiReponse(request.query.simplified);
+        let lang = setApiResponseLang(request.query.lang);
+        let simplified = simplifiedApiResponse(request.query.simplified);
         let code = request.params.code;
         response.send(
             getProvinceByCode(code, lang, simplified)
@@ -36,8 +36,8 @@
     });
 
     app.get("/provinces/code/:code", (request, response)=>{
-        let lang = setApiReponseLang(request.query.lang);
-        let simplified = simplifiedApiReponse(request.query.simplified);
+        let lang = setApiResponseLang(request.query.lang);
+        let simplified = simplifiedApiResponse(request.query.simplified);
         let code = request.params.code;
         response.send(
             getProvinceByCode(code, lang, simplified)
@@ -45,8 +45,8 @@
     });
 
     app.get("/provinces/name/:name", (request, response)=>{
-        let lang = setApiReponseLang(request.query.lang);
-        let simplified = simplifiedApiReponse(request.query.simplified);
+        let lang = setApiResponseLang(request.query.lang);
+        let simplified = simplifiedApiResponse(request.query.simplified);
         let name = request.params.name;
         response.send(
             getProvinceByName(name, lang, simplified)
